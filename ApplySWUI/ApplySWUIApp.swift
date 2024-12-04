1d9e4ec7d38fc02122554ecd16f020a388b5a343
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2024-12-04 17:31:06 -0300

    Initial Commit

---
+++ ApplySWUI/ApplySWUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  ApplySWUIApp.swift
+//  ApplySWUI
+//
+//  Created by Jose Preatorian on 04-12-24.
+//
+
+import SwiftUI
+
+@main
+struct ApplySWUIApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
